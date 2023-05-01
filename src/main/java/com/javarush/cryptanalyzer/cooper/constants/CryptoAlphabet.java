485c33397aa2889b00f656d9fb33885408eb8a5a
andrew <avcopper@mail.ru>
2023-05-02 01:17:08 +0600

    add alphabet constants

---
+++ src/main/java/com/javarush/cryptanalyzer/cooper/constants/CryptoAlphabet.java
@@ -0,0 +1,10 @@
+package com.javarush.cryptanalyzer.cooper.constants;
+
+public class CryptoAlphabet {
+    private static final String LETTERS_UPPER_CASE = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    private static final String LETTERS_LOWER_CASE = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    private static final String NUMBERS = "0123456789";
+    private static final String SYMBOLS = "., ?!-:;\"";
+    public static final String CAESAR_ALPHABET = LETTERS_UPPER_CASE + LETTERS_LOWER_CASE + NUMBERS + SYMBOLS;
+    public static final int CAESAR_ALPHABET_LENGTH = CAESAR_ALPHABET.length();
+}
