3fac75f1fa30b43e6df444ad2a0968a8bc45a458
andrew <avcopper@mail.ru>
2023-05-03 12:59:07 +0600

    add comparator

---
+++ src/main/java/com/javarush/cryptanalyzer/cooper/utils/ValueComparator.java
@@ -0,0 +1,21 @@
+package com.javarush.cryptanalyzer.cooper.utils;
+
+import java.util.Map;
+import java.util.Comparator;
+
+public class ValueComparator implements Comparator<Character> {
+    Map<Character, Integer> base;
+
+    public ValueComparator(Map<Character, Integer> base) {
+        this.base = base;
+    }
+
+    @Override
+    public int compare(Character o1, Character o2) {
+        if (base.get(o1) >= base.get(o2)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys
+    }
+}
