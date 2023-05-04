1b474934777f3ac0ceb666e1085c6eaf93440e01
andrew <avcopper@mail.ru>
2023-05-05 01:17:23 +0600

    modify exception

--- src/main/java/com/javarush/cryptanalyzer/cooper/exception/UserException.java
+++ src/main/java/com/javarush/cryptanalyzer/cooper/exception/UserException.java
@@ -1,7 +1,18 @@
 package com.javarush.cryptanalyzer.cooper.exception;
 
 public class UserException extends RuntimeException {
+    public UserException() {
+    }
+
     public UserException(String message) {
         super(message);
+    }
+
+    public UserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserException(Throwable cause) {
+        super(cause);
     }
 }
