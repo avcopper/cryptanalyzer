76e66d1d3b2ae29e5db9596ccf5ae3ae0a961ed0
andrew <avcopper@mail.ru>
2023-05-06 00:13:41 +0600

    add application

---
+++ src/main/java/com/javarush/cryptanalyzer/cooper/app/Application.java
@@ -0,0 +1,37 @@
+package com.javarush.cryptanalyzer.cooper.app;
+
+import java.io.IOException;
+import com.javarush.cryptanalyzer.cooper.utils.FunctionCode;
+import com.javarush.cryptanalyzer.cooper.controller.Controller;
+import com.javarush.cryptanalyzer.cooper.services.CryptFunction;
+
+public class Application {
+    private  final Controller controller;
+
+    public Application(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void run() {
+        controller.getView().start();
+    }
+
+    public static void crypt(String[] params) throws IOException {
+        CryptFunction cryptFunction = getCryptFunction(params[0]);
+        cryptFunction.execute(params);
+    }
+
+    private static CryptFunction getCryptFunction(String mode) {
+        return switch (mode) {
+            case "1" -> FunctionCode.valueOf("ENCODE").getCryptFunction();
+            case "2" -> FunctionCode.valueOf("DECODE").getCryptFunction();
+            case "3" -> FunctionCode.valueOf("BRUTE_FORCE").getCryptFunction();
+            case "4" -> FunctionCode.valueOf("ANALYSIS").getCryptFunction();
+            default -> FunctionCode.valueOf("UNSUPPORTED").getCryptFunction();
+        };
+    }
+
+    public void prinResult() {
+        controller.getView().printResult();
+    }
+}
