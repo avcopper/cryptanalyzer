096bef20a0f0811d80d9ecf500520822cb5a08e8
andrew <avcopper@mail.ru>
2023-05-06 00:17:59 +0600

    add constants

---
+++ src/main/java/com/javarush/cryptanalyzer/cooper/constants/Crypt.java
@@ -0,0 +1,8 @@
+package com.javarush.cryptanalyzer.cooper.constants;
+
+public class Crypt {
+    public static final String ENCRYPT = "1";
+    public static final String DECRYPT = "2";
+    public static final String BRUTE_FORCE = "3";
+    public static final String ANALYSIS = "4";
+}
