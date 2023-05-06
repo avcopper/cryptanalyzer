46b70710caf60ea829e8e8a717b6acfe661b0534
andrew <avcopper@mail.ru>
2023-05-06 23:54:48 +0600

    modify constants

--- src/main/java/com/javarush/cryptanalyzer/cooper/constants/Exception.java
+++ src/main/java/com/javarush/cryptanalyzer/cooper/constants/Exception.java
@@ -5,4 +5,5 @@ public class Exception {
     public static final String WRONG_FILE_PATH = "Wrong file path";
     public static final String WRONG_KEY = "Wrong key. Enter a positive number.";
     public static final String CODE_NOT_FOUND = "Code not found.";
+    public static final String UNSUPPORTED_OPERATION = "Unsupported operation.";
 }
