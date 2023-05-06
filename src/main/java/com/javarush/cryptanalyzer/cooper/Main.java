7bb15120741eeebd6568820952ad605fdc950585
andrew <avcopper@mail.ru>
2023-05-06 23:27:05 +0600

    modify main

--- src/main/java/com/javarush/cryptanalyzer/cooper/Main.java
+++ src/main/java/com/javarush/cryptanalyzer/cooper/Main.java
@@ -2,15 +2,21 @@ package com.javarush.cryptanalyzer.cooper;
 
 import com.javarush.cryptanalyzer.cooper.view.View;
 import com.javarush.cryptanalyzer.cooper.view.GUIView;
+import com.javarush.cryptanalyzer.cooper.entity.Result;
 import com.javarush.cryptanalyzer.cooper.app.Application;
+import com.javarush.cryptanalyzer.cooper.utils.ResultCode;
 import com.javarush.cryptanalyzer.cooper.controller.Controller;
 
 public class Main {
     public static void main(String[] args) {
-
         View view = new GUIView();
         Controller controller = new Controller(view);
         Application application = new Application(controller);
-        application.run();
+
+        try {
+            application.run();
+        } catch (Exception ex) {
+            view.showResult(new Result(ResultCode.ERROR, ex));
+        }
     }
 }
